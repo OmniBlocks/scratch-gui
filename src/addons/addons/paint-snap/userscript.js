6402778eafd0cb4fc81cd52d958c7e99a1cec3d1
supervoidcoder <88671013+supervoidcoder@users.noreply.github.com>
2025-09-23 00:59:11 +0000

    New Paint Editor

--- src/addons/addons/paint-snap/userscript.js
+++ src/addons/addons/paint-snap/userscript.js
@@ -10,15 +10,24 @@ export default async function (api) {
   addon.self.addEventListener("reenabled", enable);
   loadSettings(addon);
   const paper = await addon.tab.traps.getPaper();
-  const [tool] = paper.tools;
+
+  // Find the select tool in paper.tools (OmniBlocks-Paint may change tool order or structure)
+  let selectTool = null;
+  if (Array.isArray(paper.tools)) {
+    selectTool = paper.tools.find(t => isSelectTool(t));
+    // fallback: if not found, use first tool
+    if (!selectTool && paper.tools.length > 0) selectTool = paper.tools[0];
+  } else if (paper.tool && isSelectTool(paper.tool)) {
+    selectTool = paper.tool;
+  }
 
   toggle(addon.settings.get("enable-default"));
   setGuideColor(addon.settings.get("guide-color"));
   addon.settings.addEventListener("change", () => setGuideColor(addon.settings.get("guide-color")));
 
-  if (isSelectTool(tool)) {
-    updateSelectTool(paper, tool, addon.tab.traps.vm);
-    updateScaleTool(paper, tool, addon.tab.traps.vm);
+  if (selectTool && isSelectTool(selectTool)) {
+    updateSelectTool(paper, selectTool, addon.tab.traps.vm);
+    updateScaleTool(paper, selectTool, addon.tab.traps.vm);
   }
   initUI(api);
 }

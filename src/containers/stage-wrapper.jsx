bd12ee53cf6db257815a87b524d04fc58a428cb5
supervoidcoder <88671013+supervoidcoder@users.noreply.github.com>
2025-11-21 22:31:02 -0500

    fix: remove broken leftover jsx
    my stupid self copy pasted wrong jsx pluh 🥀

--- src/containers/stage-wrapper.jsx
+++ src/containers/stage-wrapper.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import VM from 'scratch-vm';
 import {STAGE_DISPLAY_SIZES} from '../lib/layout-constants.js';
+import {canActuallyUseWebGL} from '../lib/tw-environment-support-prober.js';
 import StageWrapperComponent from '../components/stage-wrapper/stage-wrapper.jsx';
-import WebGlBrokenModal from './webgl-broken-modal.jsx';
 
-// In the component, replace the stage canvas section:
-<Box className={styles.stageCanvasWrapper}>
-    {
-        !isRendererSupported ? null :
-        !props.canActuallyUseWebGL ? (
-            <WebGlBrokenModal isRtl={isRtl} />
-        ) : (
-            <Stage
-                stageSize={stageSize}
-                vm={vm}
-            />
-        )
-    }
-</Box>
+const StageWrapper = props => (
+    <StageWrapperComponent
+        {...props}
+        canActuallyUseWebGL={canActuallyUseWebGL()}
+    />
+);
+
+StageWrapper.propTypes = {
+    isRendererSupported: PropTypes.bool.isRequired,
+    stageSize: PropTypes.oneOf(Object.keys(STAGE_DISPLAY_SIZES)).isRequired,
+    vm: PropTypes.instanceOf(VM).isRequired
+};
+
+export default StageWrapper;

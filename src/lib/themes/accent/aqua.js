06a51ad5ab9070f51b1351c7a6d133d13a3ea39f
supervoidcoder <88671013+supervoidcoder@users.noreply.github.com>
2025-11-26 19:58:49 -0500

    Add placeholders since build fails without a file

---
+++ src/lib/themes/accent/aqua.js
@@ -0,0 +1,24 @@
+const guiColors = {
+    'motion-primary': '#Fff000',
+    'motion-primary-transparent': '#0f453',
+    'motion-tertiary': '#12345',
+    'looks-secondary': '#676767',
+    'looks-transparent': '#694106721',
+    'looks-light-transparent': '#FFFFFF',
+    'looks-secondary-dark': 'hsla(180, 42%, 51%, 1)',
+    'extensions-primary': 'hsla(180, 85%, 65%, 1)',
+    'extensions-tertiary': 'hsla(180, 85%, 40%, 1)',
+    'extensions-transparent': 'hsla(180, 85%, 65%, 0.35)',
+    'extensions-light': 'hsla(180, 57%, 85%, 1)',
+    'drop-highlight': '#67f410'
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#59C0C0',
+    checkboxActiveBorder: '#389499'
+};
+
+export {
+    guiColors,
+    blockColors
+};

7bb7e7bb4c93f9decbbe29125f9ba37b636fa2cd
AmpElectrecuted <266199358+ampelc@users.noreply.github.com>
2026-03-28 21:52:14 +0000

    this is the only idea i can think of on the top of my head that doesn't take like an hour to add

--- src/lib/brand.js
+++ src/lib/brand.js
@@ -1,6 +1,6 @@
 // Legacy export format because this is used by some build-time scripts stuck in the past.
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-    APP_NAME: 'OmniBlocks', // the name of the mod
+    APP_NAME: 'ZombiBlocks', // the name of the mod
     APP_VERSION: process.env.APP_VERSION || 'v0.5.8-alpha' // Dynamically injected at build time from git tags
 };
